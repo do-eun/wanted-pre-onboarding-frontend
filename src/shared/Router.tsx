5368f2e8d80575eac05451fa49b01f3f49914eea
doeun <drakequation@naver.com>
2023-04-09 01:56:31 +0900

    ADD: 폴더 구조 셋팅

---
+++ src/shared/Router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
+import SignUp from "../pages/SignUp";
+import SignIn from "../pages/SignIn";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
